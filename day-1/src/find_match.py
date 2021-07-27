a51f6fb3bedf573c21080223f11eb1e50aac5c65
Garrick Fernandez <20538779+garrickf@users.noreply.github.com>
2021-07-27 12:15:39 -0700

    disregard separate language folders
    * make new repos instead

--- day-1/src/find_match.py
+++ day-1/src/find_match.py
@@ -17,3 +17,18 @@ for a in expenses:
         for c in expenses:
             if a + b + c == 2020:
                 print(f"Found {a}, {b}, {c}; product: {a * b * c}")
+
+# Somewhat less naive: avoid duplicates by searching only from current index on
+def triple_sum_to_forloop_2():
+    for a_idx, a in enumerate(expenses):
+        for b_idx, b in enumerate(expenses[a_idx + 1 :]):
+            for c in expenses[b_idx + 1 :]:
+                if a + b + c == 2020:
+                    print(f"Found {a}, {b}, {c}; product: {a * b * c}")
+
+triple_sum_to_forloop_2()
+
+# Even better: a list comprehension
+# TODO
+
+# Likely the best: using a map/hashmap
